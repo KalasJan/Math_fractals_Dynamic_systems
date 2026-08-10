2dd865c17790b5e1640041aa9f4e138c1c52076d
KalasJan <70596881+KalasJan@users.noreply.github.com>
2026-08-10 16:15:31 +0200

    Lotka Voltera (Korist dravec) system

--- DS_Lotka_Voltera_Korist_Dravec.py
+++ DS_Lotka_Voltera_Korist_Dravec.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 # koeficienty (vsechny musi byt vetsi nez 0)
 
-a1 = 4 # plodnost koristi
+a1 = 3.9 # plodnost koristi
 a2 = a4 = 1 # interakce mezi koristi a dravci
 a3 = 2 # umrtnost dravcu
 b1 = b2 = 0.1 # vnitrodruhova konkurence
@@ -19,8 +19,8 @@ def mezidruh(x,y):
     return dx, dy
 
 # vypocet stredu
-stred_x = a3 / a4
-stred_y = a1 / a2
+stred_x = a3 / a4 # pocet koristi, za ktere je dravcu porad stejne (dy = 0)
+stred_y = a1 / a2 # pocet dravcu, z ktere je korist konstanti (dx = 0)
 
 # =====================================================
 
@@ -31,7 +31,7 @@ def vnitrodruh(u,v):
 
 # stred
 D = b1 * b2 + a2 * a4
-sx = (a1 * b2 + a2 * a3) / D
+sx = (a1 * b2 + a2 * a3) / D 
 sy = (-b1 * a3 + a1 * a4) / D
 
 # =====================================================
@@ -54,6 +54,9 @@ fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 7))
 # Levo - mezidruh
 ax1.streamplot(X, Y, DX_M, DY_M, color=speed_mezi, cmap='autumn', linewidth=1.2, density=1.3)
 
+ax1.axhline(stred_y, color='black', linestyle='--', linewidth=2, label='Izoklina kořisti ($dx/dt=0$)')
+ax1.axvline(stred_x, color='dimgray', linestyle='-.', linewidth=2, label='Izoklina dravců ($dy/dt=0$)')
+
 ax1.scatter([stred_x], [stred_y], color='black', s=120, zorder=5, 
            label=f'Kritický bod (Střed) [{stred_x:.1f}, {stred_y:.1f}]')
 ax1.set_title('Bez vnitrodruhové konkurence', fontsize=12)
@@ -65,11 +68,22 @@ ax1.legend(loc='upper right')
 # Pravo - i vnitroddruhova konkurence
 ax2.streamplot(X, Y, DX_V, DY_V, color=speed_vnit, cmap='winter', linewidth=1.2, density=1.3)
 
+# izokliny (du = 0, dv = 0)
+izo_u = (a1 - b1 * osa_x) / a2 # vliv dravců na stagnaci kořisti při vnitrodruhové konkurenci
+izo_v = (-a3 + a4 * osa_x) / b2 # minimální kořist nutná pro růst populace dravců při jejich vlastní konkurenci
+
+ax2.plot(osa_x, izo_u, color='black', linestyle='--', linewidth=2, label='Izoklina kořisti ($du/dt=0$)')
+ax2.plot(osa_x, izo_v, color='dimgray', linestyle='-.', linewidth=2, label='Izoklina dravců ($dv/dt=0$)')
+
+
 ax2.scatter([sx], [sy], color='black', s=120, zorder=5, 
            label=f'Kritický bod [{sx:.1f}, {sy:.1f}]')
 ax2.set_title('S vnitrodruhovou konkurencí (Stabilizace ekosystému)', fontsize=12)
 ax2.set_xlabel('Populace kořisti ($u$)', fontsize=11)
 ax2.set_ylabel('Populace dravců ($v$)', fontsize=11)
+
+ax2.set_xlim(0, 8)
+ax2.set_ylim(0, 8)
 ax2.grid(True, linestyle=':', alpha=0.5)
 ax2.legend(loc='upper right')
 
